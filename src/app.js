1c375b9647b1728c2017b78d4cfd9b1450915f49
Oleg Ledvanov <olegledvanov@gmail.com>
2021-03-21 00:21:58 +0300

    add auth feature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
-console.log("Hello, app!");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/style.css";
+import taskFieldTemplate from "./templates/taskField.html";
+import noAccessTemplate from "./templates/noAccess.html";
+import { User } from "./models/User";
+import { generateTestUser } from "./utils";
+import { State } from "./state";
+import { authUser } from "./services/auth";
+
+export const appState = new State();
+
+const loginForm = document.querySelector("#app-login-form");
+
+generateTestUser(User);
+
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(loginForm);
+  const login = formData.get("login");
+  const password = formData.get("password");
+
+  let fieldHTMLContent = authUser(login, password)
+    ? taskFieldTemplate
+    : noAccessTemplate;
+
+  document.querySelector("#content").innerHTML = fieldHTMLContent;
+});
