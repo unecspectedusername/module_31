81eb74950e55d8337534e02ff06ef78b201185fd
Oleg Ledvanov <olegledvanov@gmail.com>
2021-03-20 22:53:29 +0300

    add getFromStorage, addToStorage, generateTestUser to utils

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+export const getFromStorage = function (key) {
+  return JSON.parse(localStorage.getItem(key) || "[]");
+};
+
+export const addToStorage = function (obj, key) {
+  const storageData = getFromStorage(key);
+  storageData.push(obj);
+  localStorage.setItem(key, JSON.stringify(storageData));
+};
+
+export const generateTestUser = function (User) {
+  localStorage.clear();
+  const testUser = new User("test", "qwerty123");
+  User.save(testUser);
+};
